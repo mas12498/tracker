a273c137540f30d3e871cacc38fb9bd8eccddb21
Casey <caseyorigami@gmail.com>
2018-09-13 03:09:44 -0700

    Added file output of the solution

--- src/tspi/controller/MonteCarloTest.java
+++ src/tspi/controller/MonteCarloTest.java
@@ -1,15 +1,13 @@
 package tspi.controller;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
+import java.io.PrintStream;
 import java.util.List;
 import java.util.Random;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
-import rotation.Vector3;
+import rotation.Angle;
 import tspi.model.Pedestal;
 import tspi.model.PedestalModel;
 import tspi.model.Polar;
@@ -17,60 +15,74 @@ import tspi.model.Solution;
 import tspi.model.Target;
 import tspi.model.TargetModel;
 
-/** So I'm not to happy with the architecture, and it's probably my fault;
- * Mainly because Solution got conflated with Target to make some view tasks convenient.
- * Since all these iterations are little one-off demonstrators, I wasn't too worried about it- however;
- * We should really take careful stock of our architecture before starting a large NRT application.
+/** I'm getting confused on the architecture; I think Solution got conflated with 
+ * Target to make some view tasks convenient. Probably my fault. Since all these 
+ * iterations are one-off demonstrators, I wasn't too worried about it- however;
+ * We should really take careful stock of our architecture before starting a 
+ * large NRT application.
  * 
- * To avoid kludging to meet another demonstrator or deep conflicts with the master branch,
- * I just wrote stuff rather procedurally here to avoid messing with the model objects right now in interest of getting the data out...
+ * To avoid kludging to meet another demonstrator or deep conflicts with the 
+ * master branch, I just wrote stuff rather procedurally here to avoid messing
+ * with the model objects right now in interest of getting the data out...
  */
 public class MonteCarloTest {
 	
-	private static Random Generator;
-
+	// note our random number generator always uses the same seed for repeatability...
+	private static Random Generator = new Random( 1L );
+	// TODO might want to use the commons distributions API instead...
+	 
 	public static void main(String args[]) {
-		long seed = 1;
+		// Adjust these values; let me know if this is what you need.
 		long time = 0;
 		double lat = 0.0, lon = 0.0, height=0.0;
 		double step = 0.05;
-		int count = 24, trials = 1000;
+		int count = 24, trials = 100;
+		String input = ".\\data\\pedestalsWithOrigin.csv"; //"C:\Users\shiel\Documents\workspace\tracker\data\pedestalsWithOrigin.csv"
+		String output = ".\\data\\GridTest100.csv";
 		
-		Generator = new Random( seed );
-		// TODO probably want to use the commons distributions API instead...
+		// I need the origin as a pedestal, so I can point it and compute spherical errors against Solutions.
+		// you did want spherical errors from the synthetic origin right? Not EFG?
+		Pedestal origin = new Pedestal( "origin", true, true, 
+				Angle.inDegrees(0.0), Angle.inDegrees(0.0), 0.0);
 		
-		ArrayList<Pedestal> array;		
+		// I don't understand the origin name convention; is it tied specifically to increment3 for being able to change origin on selection? 
+		// I think we should just add a header bar that lets you select it from a drop down whose list model is derived from the pedestal model... 
+		// I don't know, we'll talk it through.
+		
+		// I don't think I need this, because I'm computing all errors by pointing an origin pedestal...
+		//Pedestal.setOrigin( origin.getLocation() );
+		// I feel the origin should be an explicit argument more than a global state...
+		// for example, to get local coordinates you might add pedestal.getRelative(origin)
+		// then whatever application is running it would track the origin, whether it is a gui or Solution etc...
+		// this would prevent subtle global interactions, and reduces the complexity of Pedestal...
+		
 		PedestalModel model = new PedestalModel();
-		File input = new File("");
-		
 		try {
-			model.load(input);
-			array = model.asList();
-			
-			Pedestal origin = model.getPedestal(0); // or something...
-			
-//			double grid[][][][] = 
+			PrintStream stream = new PrintStream( new File(output) );
+			model.load( new File(input) );
 			testGrid(
-					time, lat, lon, height, step,
-					origin, model.asList(),
-					count, trials);
+					time, lat, lon, height,
+					step, count, trials, 
+					origin, model.asList(), stream);
 			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-
-		
 	}
 	
 	public static void testGrid(
-			long time, double lat, double lon, double height, double step,
-			Pedestal origin, List<Pedestal> pedestals,
-			int count, int trials ) {
-
+			long time, double lat, double lon, double height,
+			double step, int count, int trials, 
+			Pedestal origin, List<Pedestal> pedestals, PrintStream stream )
+	{
+		// print the header
+		stream.append("Lat, Lon, H, AZmin, AZmean, AZmode, AZmax, AZdev, ELmin, ELmean, ELmode, ELmax, ELdev, Rmin, Rmean, Rmode, Rmax, Rdev\n");
+		
 		// declare statistics for az, el, r in that order
 		DescriptiveStatistics statistics[] = new DescriptiveStatistics[3];
-		for (DescriptiveStatistics statistic : statistics)
-			statistic = new DescriptiveStatistics(); // unfortunately there is no lib for multivariate descripive stats -i.e. stats containing in memory percentile methods
+		for (int n=0; n<3; n++)
+			statistics[n] = new DescriptiveStatistics();
+		// unfortunately there is no lib for multivariate descripive stats -i.e. stats containing in memory percentile methods
 		
 		// generate a grid of targets
 		TargetModel model = generateTargetGrid( time, lat, lon, height, count, step );
@@ -82,7 +94,7 @@ public class MonteCarloTest {
 			for (DescriptiveStatistics statistic:statistics)
 				statistic.clear();
 			
-			// compute the correct, unperturbed location
+			// compute the correct, unperturbed pointing coordinates
 			origin.pointToLocation( target.getGeocentricCoordinates() );
 			Polar truth = origin.getLocal();
 
@@ -93,9 +105,11 @@ public class MonteCarloTest {
 				pointPedestals(pedestals, target);
 				
 				// Compute a target solution, and point the origin at it 
-				Solution solution = new Solution( pedestals ); // TODO make sure origin is right in this solution!!!
+				Solution solution = new Solution( pedestals );
 				origin.pointToLocation( solution.position_EFG );
 				Polar measurement = origin.getLocal();
+				// TODO make sure origin is right in this solution!!!
+				// TODO Solution pretty much needs to start from scratch every time right? I shouldn't be re-using something right?
 				
 				// Now compute error in spherical terms
 				double error[] = {
@@ -107,16 +121,32 @@ public class MonteCarloTest {
 				// and update the descriptive statistics
 				for (int n=0; n<3; n++)
 					statistics[n].addValue(error[n]);
-			} // TODO SummaryStatistics has versions with contributing hierarchies and synchronization if we want to try getting a multithreading speed boost for larger tests!
+				// TODO SummaryStatistics has versions with contributing hierarchies and synchronization;
+				// if we want to try getting a multi-threading speed boost for larger tests...
+				// hell, it might be feasible to progressively refine a plot as trials are run if we make one of these per grid point...
+			}
 			
-			// compute min, mean, mode and max of errors in AER
-			statistics[0].getMean();
-			// TODO store the error in the target grid? attached to some internal solution?
-			// or in a 2d array?
-
+			// print out the min, mean, mode and max of errors in AER
+			Character separator = ',';
+			stream.append( Double.toString( target.getLatitude().getDegrees() ) );
+			stream.append( separator );
+			stream.append( Double.toString( target.getLongitude().getDegrees() ) );
+			stream.append( separator );
+			stream.append( Double.toString( target.getHeight() ) );
+			for (int i=0; i<3; i++) {
+				stream.append( separator );
+				stream.append( Double.toString( statistics[i].getMin() ) );
+				stream.append( separator );
+				stream.append( Double.toString( statistics[i].getMean() ) );
+				stream.append( separator );
+				stream.append( Double.toString( statistics[i].getPercentile(50.0) ) );
+				stream.append( separator );
+				stream.append( Double.toString( statistics[i].getMax() ) );
+				stream.append( separator );
+				stream.append( Double.toString( statistics[i].getStandardDeviation() ) );
+			}
+			stream.append("\r\n"); // TODO should we extract more info from Solution too?
 		}
-			
-		
 	}
 	
 	public static TargetModel generateTargetGrid(
@@ -124,9 +154,11 @@ public class MonteCarloTest {
 			int count, double step
 	) {
 		TargetModel model = new TargetModel();
-		for (int n=-count; n<count; n++) { 
-			Target target = new Target(time, lat+(count*step), lon+(count*step), height);
-			model.add(model.getRowCount(), target);
+		for (int i=-count; i<=count; i++) {
+			for (int j=-count; j<=count; j++) {
+				Target target = new Target(time, lat+(i*step), lon+(j*step), height);
+				model.add(model.getRowCount(), target);
+			}
 		}
 		return model;
 	}
@@ -138,24 +170,38 @@ public class MonteCarloTest {
 			pedestal.point(pertubed);
 		}
 	}
-	
-	class GridAccuracy {
-		final int azimuth=0, elevation=1, range=2;
-		final int min=0, mean=1, mode=2, max=3, deviation=4;
-		double stat[][] = new double [3][5];
-		long time;
-		double lat, lon, height;
-	}
-	
-	public static double ftime( Date time ) {
-		Calendar c = Calendar.getInstance();
-		c.setTime( time );
-		double millis = 0.0;
-		millis += c.get(Calendar.HOUR_OF_DAY)*3600000;
-		millis += c.get(Calendar.MINUTE)*60000;
-		millis += c.get(Calendar.SECOND)*1000;
-		millis += c.get(Calendar.MILLISECOND);
-		return millis/1000.0;
-	}
+
+	// meh, we might eventually want to produce some model for these results when we start passing them to a gui...
+//	class GridAccuracy {
+//		long time;
+//		Angle lat, lon;
+//		double height;
+//		
+//		final int azimuth=0, elevation=1, range=2;
+//		final int min=0, mean=1, mode=2, max=3, deviation=4;
+//		double stat[][] = new double [3][5];
+//				
+//		GridAccuracy( Target target, DescriptiveStatistics statistics[] ) {
+//			time = target.getTime();
+//			lat = target.getLatitude();
+//			lon = target.getLongitude();
+//			height = target.getHeight();
+//			stat[azimuth][min] = statistics[azimuth].getMin();
+//			stat[azimuth][mean] = statistics[azimuth].getMean();
+//			stat[azimuth][mode] = statistics[azimuth].getPercentile(50.0);
+//			stat[azimuth][max] = statistics[azimuth].getMax();
+//			stat[azimuth][deviation] = statistics[azimuth].getStandardDeviation();
+//			stat[elevation][min] = statistics[elevation].getMin();
+//			stat[elevation][mean] = statistics[elevation].getMean();
+//			stat[elevation][mode] = statistics[elevation].getPercentile(50.0);
+//			stat[elevation][max] = statistics[elevation].getMax();
+//			stat[elevation][deviation] = statistics[elevation].getStandardDeviation();
+//			stat[range][min] = statistics[range].getMin();
+//			stat[range][mean] = statistics[range].getMean();
+//			stat[range][mode] = statistics[range].getPercentile(50.0);
+//			stat[range][max] = statistics[range].getMax();
+//			stat[range][deviation] = statistics[range].getStandardDeviation();
+//		}
+//	}
 }
 

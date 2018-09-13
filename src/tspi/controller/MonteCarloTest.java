eeac8d371c3ffa29162e8fbf09781a933afa652a
caseyshields <caseyorigami@gmail.com>
2018-09-12 17:11:55 -0700

    fleshed out stat gathering
    working on intermediate grid stat representation

--- src/tspi/controller/MonteCarloTest.java
+++ src/tspi/controller/MonteCarloTest.java
@@ -1,31 +1,95 @@
 package tspi.controller;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.PrintStream;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
-import rotation.Angle;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
 import rotation.Vector3;
-import tspi.model.Ellipsoid;
 import tspi.model.Pedestal;
-import tspi.model.PedestalModel;
 import tspi.model.Polar;
 import tspi.model.Solution;
 import tspi.model.Target;
 import tspi.model.TargetModel;
 
+/** So I'm not to happy with the architecture, and it's probably my fault;
+ * Mainly because Solution got conflated with Target to make some view tasks convenient.
+ * Since all these iterations are little one-off demonstrators, I wasn't too worried about it- however;
+ * We should really take careful stock of our architecture before starting a large NRT application.
+ * 
+ * To avoid kludging to meet another demonstrator or deep conflicts with the master branch,
+ * I just wrote stuff rather procedurally here to avoid messing with the model objects right now in interest of getting the data out...
+ */
 public class MonteCarloTest {
+	
+	private static Random Generator;
 
+	public static void main(String args[]) {
+		long seed = 1;
+		Generator = new Random( seed ); // TODO probably want to use the commons distributions API instead...
+		
+	}
+	
+	public static void testGrid(
+			long time, double lat, double lon, double height, double step,
+			Pedestal origin, List<Pedestal> pedestals,
+			int count, int trials ) {
+
+		// declare statistics for az, el, r in that order
+		DescriptiveStatistics statistics[] = new DescriptiveStatistics[3];
+		for (DescriptiveStatistics statistic : statistics)
+			statistic = new DescriptiveStatistics(); // unfortunately there is no lib for multivariate descripive stats -i.e. stats containing in memory percentile methods
+		
+		// generate a grid of targets
+		TargetModel model = generateTargetGrid( time, lat, lon, height, count, step );
+		
+		// For every target on that grid
+		for (Target target: model) {
+			
+			// initialize stats for new round of trials
+			for (DescriptiveStatistics statistic:statistics)
+				statistic.clear();
+			
+			// compute the correct, unperturbed location
+			origin.pointToLocation( target.getGeocentricCoordinates() );
+			Polar truth = origin.getLocal();
+
+			// for every trial
+			for( int trial=0; trial < trials; trial++ ) {
+				
+				// point the pedestals at the target with a random perturbation generated from their error model
+				pointPedestals(pedestals, target);
+				
+				// Compute a target solution, and point the origin at it 
+				Solution solution = new Solution( pedestals ); // TODO make sure origin is right in this solution!!!
+				origin.pointToLocation( solution.position_EFG );
+				Polar measurement = origin.getLocal();
+				
+				// Now compute error in spherical terms
+				double error[] = {
+						measurement.getAzimuth().subtract( truth.getAzimuth() ).getDegrees(),
+						measurement.getElevation().subtract( truth.getElevation() ).getDegrees(),
+						measurement.getRange() - truth.getRange()
+				};
+				
+				// and update the descriptive statistics
+				for (int n=0; n<3; n++)
+					statistics[n].addValue(error[n]);
+			} // TODO SummaryStatistics has versions with contributing hierarchies and synchronization if we want to try getting a multithreading speed boost for larger tests!
+			
+			// compute min, mean, mode and max of errors in AER
+			statistics[0].getMean();
+			// TODO store the error in the target grid? attached to some internal solution?
+			// or in a 2d array?
+
+		}
+			
+		
+	}
+	
 	public static TargetModel generateTargetGrid(
 			long time, double lat, double lon, double height,
 			int count, double step
@@ -38,7 +102,7 @@ public class MonteCarloTest {
 		return model;
 	}
 	
-	public static void pointPedestals( PedestalModel model, Target target) {
+	public static void pointPedestals( List<Pedestal> model, Target target) {
 		for (Pedestal pedestal : model) {
 			pedestal.pointToLocation( target.getGeocentricCoordinates() );
 			Polar pertubed = pedestal.getPerturbedLocal( Generator );
@@ -46,352 +110,10 @@ public class MonteCarloTest {
 		}
 	}
 	
-	public static void testGrid(
-			long time, double lat, double lon, double height, double step,
-			List<Pedestal> pedestals, int count, int trials ) {
-		
-//		File file = new File("pedestal.txt");
-//		PedestalModel pedestals = new PedestalModel();
-//		try {
-//			pedestals.load( file );
-//		} catch(Exception exception) {
-//			
-//		}
-			
-		TargetModel model = generateTargetGrid( time, lat, lon, height, count, step );
-		for (Target target: model) {
-			
-			Vector3 truth = target.getGeocentricCoordinates();
-			
-			ArrayList<Vector3> errors = new ArrayList<Vector3>(trials);
-			
-			for( int n=0; n < trials; n++ ) {
-				pointPedestals(pedestals, target);
-				
-				Solution solution = new Solution( pedestals );
-				Vector3 error = solution.position_EFG.subtract(truth);
-				
-				errors.add(error);
-			}
-			
-			// compute min, mean, mode and max of errors in AER
-			for ( Vector3 error : errors ) {
-				
-			}
-			
-			//store the error in the target grid? attached to some internal solution?
-			// or in a 2d array?
-
-		}
-			
-		
-	}
-	
-	
-	static SimpleDateFormat dayFormat = new SimpleDateFormat("yyyy.DDD");
-	static SimpleDateFormat timeFormat = new SimpleDateFormat("HHmmss.SSS");
-	static NumberFormat numberFormat = new DecimalFormat("0.00000000");
-	static NumberFormat fTimeFormat = new DecimalFormat("00000000.000");
-	static NumberFormat longitudeFormat = new DecimalFormat("0.000000000000");
-	static NumberFormat latitudeFormat = new DecimalFormat("0.000000000000");
-//	static NumberFormat longitudeFormat = new DecimalFormat("0.000000000000e;-0.000000000000w");
-//	static NumberFormat latitudeFormat = new DecimalFormat("0.000000000000n;-0.000000000000s");
-	static String Separator = ",";
-	static String Line = "\r\n";
-	
-	static Random Generator = new Random(1);
-	
-	public static void main(String[] args) {
-		try {
-			if(args.length>0 && args[0].equalsIgnoreCase("-generate")) {
-				File pedestalFile = new File(args[1]);//"C:\\Users\\Casey\\Documents\\pedestals.csv");
-				File targetFile = new File(args[2]);//"C:\\Users\\Casey\\Documents\\targets.csv");
-				File ensembleFile = new File(args[3]);//"C:\\Users\\Casey\\Documents\\ensemble.csv");
-				
-				int trials = 1;
-				
-				PrintStream ensembleStream = //System.out;
-						new PrintStream(
-						new BufferedOutputStream(
-						new FileOutputStream( ensembleFile )));
-				
-				MonteCarloTest.generate(
-						Calendar.getInstance().getTime(),
-						100,
-						pedestalFile,
-						targetFile,
-						trials,
-						ensembleStream,
-						true,
-						0.0, 0.0,
-						0.0, 0.0);////1.0, 1.0);
-				
-			} else if(args.length>0 && args[0].equalsIgnoreCase("-solve")) {
-				File pedestalFile = new File(args[1]);//"C:\\Users\\Casey\\Documents\\pedestals.csv");
-				File ensembleFile = new File(args[2]);//"C:\\Users\\Casey\\Documents\\ensemble.csv");
-				File solutionFile = new File(args[3]);//"C:\\Users\\Casey\\Documents\\solution.csv");
-				int solutionCount = Integer.parseInt(args[4]);
-				boolean header = (args[5].equalsIgnoreCase("true")) ? true : false;
-				
-				PrintStream solutionStream = //System.out;
-						new PrintStream(
-						new BufferedOutputStream(
-						new FileOutputStream( solutionFile )));
-				
-				MonteCarloTest.solve(
-						ensembleFile,
-						pedestalFile,
-						solutionCount, //4,
-						solutionStream,
-						header);//true);
-			} else {
-				System.out.println(
-						"Usage:\n"
-						+ "EnsembleTest -generate <input pedestal path> <input targets path> <output ensemble path>\n"
-						+ "or\n"
-						+ "EnsembleTest -solve <input pedestal path> <input ensemble path> <ouput solution path> <solution pedestal count> <header present={true|else}>\n"
-								);
-			}
-			
-		} catch(Exception exception) {
-			exception.printStackTrace();
-		}
-	}
-	
-	public static void generate(
-			Date time,
-			int dt,
-			File pedestalFile, // ensemble file's pedestal order will correspond to this pedestal file
-			File targetFile,
-			int trialsPerTarget,
-			PrintStream stream,
-			boolean header,
-			double azBias, double elBias, //TODO add these values to Pedestal, and load them per pedestal!
-			double azRange, double elRange )
-			throws Exception
-	{
-		PedestalModel pedestals = new PedestalModel();
-		pedestals.load( pedestalFile );
-		TargetModel targets = new TargetModel();
-		targets.load( targetFile );
-		MonteCarloTest.generate(
-				time, dt, pedestals, targets, trialsPerTarget, stream, header, azBias, elBias, azRange, elRange );
-	}
-	
-	public static void generate(
-			Date time, // start time
-			int dt, // trial time step in milliseconds
-			PedestalModel pedestals,
-			TargetModel targets,
-			int trialsPerTarget,
-			PrintStream stream,
-			boolean header,
-			double azBias, double elBias, //TODO add these values to Pedestal, and load them per pedestal!
-			double azRange, double elRange)
-	{
-		// print header
-		if( header ) {
-			stream.append( "Day" ); stream.append( Separator );
-			stream.append( "Time" ); stream.append( Separator );
-			stream.append( "FTime" ); stream.append( Separator );
-			for(Pedestal pedestal : pedestals) {
-				stream.append( pedestal.getSystemId()+"_az" ); stream.append( Separator );
-				stream.append( pedestal.getSystemId()+"_el" ); stream.append( Separator );
-			}
-			//TODO truths?
-			stream.append(Line);
-		}
-		
-		for( Target target : targets ) {
-			for( int trial=0; trial<trialsPerTarget; trial++ ) {
-			
-				// increase time every trial?
-				time.setTime( time.getTime() + dt);
-				
-				// write time columns
-				stream.append( dayFormat.format(time) );
-				stream.append( Separator );
-				stream.append( timeFormat.format(time) );
-				stream.append( Separator );
-				stream.append( fTimeFormat.format( ftime(time) ) );
-				stream.append( Separator );
-				
-				// TODO just generate pedestal measurements in the same order they appear in the pedestal file.
-//				// TODO we could set these using column headers instead of repeating them each record...
-//				for( Pedestal pedestal : pedestals ) {
-//					stream.append( pedestal.getSystemId() );		
-//					stream.append( Separator );
-//				} // or they could be specified by a pedestal file...
-				
-				for( Pedestal pedestal : pedestals ) {
-					
-					// point pedestal at target then perturb
-					pedestal.pointToLocation( target.getGeocentricCoordinates() ); // TODO might want to cache instead of recomputing every trial...
-					Angle azimuth = pedestal.getLocal().getAzimuth();
-					Angle elevation = pedestal.getLocal().getElevation();
-					azimuth.add( Angle.inDegrees( azBias + (Math.random()*azRange) ) );
-					elevation.add( Angle.inDegrees( elBias + (Math.random()*elRange) ) );
-					pedestal.pointDirection(azimuth, elevation);
-					
-					// TODO get error model from pedestal instead of args!
-					// TODO support gaussian error instead of just linear...
-					// TODO polar or rectangular coords?
-					
-//					stream.append( numberFormat.format( pedestal.getRange() ) );		
-//					stream.append( Separator );
-					stream.append( numberFormat.format( pedestal.getLocal().getAzimuth().getDegrees() ) );		
-					stream.append( Separator );
-					stream.append( numberFormat.format( pedestal.getLocal().getElevation().getDegrees() ) );		
-					stream.append( Separator );
-				}
-				
-				//TODO Eh? Use truth for which pedestal?
-				
-				stream.append( Line );
-			}
-		}
-		stream.close();
-	}
-	
-	public static void solve( 
-			File ensembleFile,
-			File pedestalFile,
-			int pedestalCount,
-			PrintStream stream,
-			boolean header)
-			throws Exception
-	{
-		PedestalModel pedestals = new PedestalModel();
-		pedestals.load( pedestalFile );
-		MonteCarloTest.solve( 
-				ensembleFile,
-				pedestals,
-				pedestalCount,
-				stream,
-				header);
-	}
-	public static void solve( 
-			File ensembleFile,
-			PedestalModel pedestals, // pedestal order corresponds to pedestal order in ensemble file...
-			int pedestalCount, // the first n columns are used in the solution
-			PrintStream stream,
-			boolean header)
-			throws Exception
-	{
-		BufferedReader reader = new BufferedReader( new FileReader(ensembleFile) );
-		String line = "";
-		
-		// consume ensemble header and output solution header
-		if(header) {
-			line = reader.readLine();
-			stream.append( "Day" ); stream.append( Separator );
-			stream.append( "Time" ); stream.append( Separator );
-			stream.append( "FTime" ); stream.append( Separator );
-			stream.append( "E" ); stream.append( Separator );
-			stream.append( "F" ); stream.append( Separator );
-			stream.append( "G" ); stream.append( Separator );
-			stream.append( "long" ); stream.append( Separator );
-			stream.append( "lat" ); stream.append( Separator );
-			stream.append( "h" ); stream.append( Separator );
-			stream.append( "condition" ); stream.append( Separator );
-			for(Pedestal pedestal : pedestals) {
-				stream.append( pedestal.getSystemId()+"_az" ); stream.append( Separator );
-				stream.append( pedestal.getSystemId()+"_el" ); stream.append( Separator );
-				stream.append( pedestal.getSystemId()+"_ra" ); stream.append( Separator );
-			}
-			stream.append(Line);
-		}
-		
-		int count = 1;
-		try {
-			while( (line=reader.readLine()) != null) {
-				String cols[] = line.split( Separator );
-				if(cols.length==0 || (cols.length==1&&cols[0].equals("")))
-					continue;
-				
-				// write the time to output
-				String day = cols[0].trim();
-				String time = cols[1].trim();
-				Double ftime = Double.parseDouble(cols[2].trim());
-				// TODO compute a time representation which is useful...
-				stream.append( day );
-				stream.append(Separator);
-				stream.append( time );
-				stream.append(Separator);
-				stream.append( fTimeFormat.format( ftime ) );
-				stream.append(Separator);
-				
-				// just get this info from the pedestal file and assume the pedestal order is the same!!!
-//				// parse system names
-//				if( pedestals.getColumnCount()==0 )
-//					for(int index=0; index<pedestalCount; index++) {
-				// TODO how do we get location? we have to load a pedestal file anyways!
-//					Pedestal pedestal = new Pedestal(cols[3+index], Angle.inDegrees(0.0), Angle.inDegrees(0.0), 0.0);
-//					pedestals.add(index, pedestal);
-//				}
-				
-				// point the pedestals using the ensemble measurements
-				for( int n=0; n<pedestals.getRowCount(); n++) {
-					Pedestal pedestal = pedestals.getPedestal(n);
-					Angle azimuth = Angle.inDegrees( Double.parseDouble( cols[3+n*2].trim() ) );
-					Angle elevation = Angle.inDegrees( Double.parseDouble( cols[4+n*2].trim() ) );
-//					double range = Double.parseDouble( cols[3+n].trim() ); // TODO number of sensor will have to come from the pedestal file as well...
-					pedestal.pointDirection(/*range,*/ azimuth, elevation);
-				}
-				
-				// solve the system
-				ArrayList<Pedestal> list = new ArrayList<Pedestal>();
-				for( int n=0; n<pedestalCount; n++  )
-					list.add(pedestals.getPedestal(n));
-				Vector3 origin = pedestals.getPedestal(0).getLocation();
-				Solution solution = new Solution( list );
-				Ellipsoid coordinate = new Ellipsoid();
-				coordinate.setGeocentric( solution.position_EFG );
-				double condition = solution.condition;
-				
-				// write the solution coordinates
-				stream.append( numberFormat.format( solution.position_EFG.getX() ) ); // todo might want to make formats for longitude and latitude
-				stream.append( Separator );
-				stream.append( numberFormat.format( solution.position_EFG.getY() ) );
-				stream.append( Separator );
-				stream.append( numberFormat.format( solution.position_EFG.getZ() ) );
-				stream.append( Separator );
-				stream.append( longitudeFormat.format( coordinate.getEastLongitude().getDegrees() ) ); // todo might want to make formats for longitude and latitude
-				stream.append( Separator );
-				stream.append( latitudeFormat.format( coordinate.getNorthLatitude().getDegrees() ) );
-				stream.append( Separator );
-				stream.append( numberFormat.format( coordinate.getEllipsoidHeight() ) );
-				stream.append( Separator );
-				stream.append( numberFormat.format( condition ) );
-				stream.append( Separator );
-				
-				// compute the pedestal az and el given the solution
-				for(Pedestal pedestal : pedestals) {
-					pedestal.pointToLocation( solution.position_EFG );
-					double azimuth = pedestal.getLocal().getAzimuth().getDegrees();
-					double elevation = pedestal.getLocal().getElevation().getDegrees();
-					double range = pedestal.getLocal().getRange();
-					
-					// add solution direction to output
-					stream.append( numberFormat.format( azimuth ) );
-					stream.append( Separator );
-					stream.append( numberFormat.format( elevation ) );
-					stream.append( Separator );
-					stream.append( numberFormat.format( range ) );
-					stream.append( Separator );
-				}
-				
-				stream.append( Line );
-				
-				count++;
-			}
-		} catch(Exception exception) {
-			exception.printStackTrace();
-			throw new Exception( "Error, line "+count+"("+line+") : "+exception.getMessage() );
-		} finally {
-			reader.close();
-			stream.close();
-		}
+	class GridAccuracy {
+		final int azimuth=0, elevation=1, range=2;
+		final int min=0, mean=1, mode=2, max=3, deviation=4;
+		double stat[][] = new double [3][5];
 	}
 	
 	public static double ftime( Date time ) {
@@ -406,14 +128,3 @@ public class MonteCarloTest {
 	}
 }
 
-//example solver arguments;
-//-solve
-//C:\\Users\\Casey\\Documents\\pedestals.csv
-//C:\\Users\\Casey\\Documents\\ensemble.csv
-//C:\\Users\\Casey\\Documents\\solution.csv
-//
-///home/mike/repos/mike/pedestalsExample.csv
-///home/mike/repos/mike/ensemble.csv
-///home/mike/repos/mike/solution.csv
-//4
-//true

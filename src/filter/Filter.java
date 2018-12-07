d8ddf819ac726fb6237e02cf7e14175dda32709f
mshields <mshields@OLN-GR7LHL2.internal.oln-afmc.af.mil>
2018-12-06 19:19:15 -0800

    added more filter matrices

--- src/filter/Filter.java
+++ src/filter/Filter.java
@@ -2,11 +2,11 @@ package filter;
 
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.ArrayRealVector;
-import org.apache.commons.math3.linear.DecompositionSolver;
 import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.QRDecomposition;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.linear.SparseRealMatrix;
 
 import tspi.model.Pedestal;
 import tspi.model.Polar;
@@ -18,34 +18,52 @@ public class Filter {
 	RealMatrix covariance;
 	RealVector state;
 		
-	RealVector x;
-	RealMatrix P;
+	RealMatrix x = MatrixUtils.createRealMatrix(9,1);
+	RealMatrix P = MatrixUtils.createRealMatrix(9,9);
 	
-	RealMatrix x_;
-	RealMatrix P_;
+	RealMatrix x_ = MatrixUtils.createRealMatrix(9,1);
+	RealMatrix P_ = MatrixUtils.createRealMatrix(9,9);
 	
 	
-	double dt;
-	RealVector A; //transition matrix: apriori predict from time k-1 to time at k
-	RealMatrix Q; //process noise matrix: stochastic noise introduced in transition
+	double dt=0.020;
+	
+	SparseRealMatrix A; //transition matrix: apriori predict from time k-1 to time at k
+	
+	double wn = 10;
+	double[] qDiagnonal = {0,0,0,0,0,0,wn,wn,wn};
+	SparseRealMatrix Q = (SparseRealMatrix) MatrixUtils.createRealDiagonalMatrix(qDiagnonal); //.createRealIdentityMatrix(9);  //process noise matrix: stochastic noise introduced in transition
 
-	RealMatrix H; //observation matrix: map from state-space x to measurement space of z
-	RealMatrix z; //measurement vector for updating apriori filter
-	RealMatrix R; //measurement noise matrix: stochastic noise introduced in measurement
+	RealMatrix H = MatrixUtils.createRealMatrix(6,9); //observation matrix: map from state-space x to measurement space of z
+	RealMatrix z = MatrixUtils.createRealMatrix(6,1); //measurement vector for updating apriori filter
+	double sa = StrictMath.toRadians(.005);
+	double se = StrictMath.toRadians(.005);
+	double[] rDiagnonal = {sa,se,sa,se,sa,se};
+	SparseRealMatrix R = (SparseRealMatrix) MatrixUtils.createRealDiagonalMatrix(rDiagnonal); //measurement noise matrix: stochastic noise introduced in measurement
 	
 	RealMatrix K; //Kalman gain and blending matrix.
 
 	/** 
+	 * Generate one of these for each likely propagation interval and on fly for weird ones.
+	 * 
+	 * A: generate sparse transition matrix from time of propagation
+	 * @param sec_propagate   */
+    public SparseRealMatrix createTransitionMatrix(double timePropagate) {
+    	RealMatrix subIdentity = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(timePropagate);
+    	RealMatrix T = MatrixUtils.createRealIdentityMatrix(9);
+    	T.setSubMatrix(subIdentity.getData(), 0, 3);
+    	T.setSubMatrix(subIdentity.getData(), 6, 3);
+    	subIdentity.scalarMultiply(timePropagate/2);
+    	T.setSubMatrix(subIdentity.getData(), 0, 6);
+    	return ( SparseRealMatrix) T; //cast to sparse!
+    }
+	
+	
+	/** 
 	 * K: measurement update Kalman gain and blending matrix
 	 * @param H,R,P_   */
-	public RealMatrix updateKalmanGain( RealMatrix H, RealMatrix R, RealMatrix P_ ) {
-		//RealMatrix numerator = P_.multiply(H.transpose());                        //matrix RHS
-		//RealMatrix denominator =( H.multiply(P_).multiply(H.transpose())).add(R); //matrix LHS				
-		//QRDecomposition factored = new QRDecomposition((H.multiply(P_).multiply(H.transpose())).add(R));		
-		//DecompositionSolver K = factored.getSolver();                             //solve K
-		//return 	factored.getSolver().solve(P_.multiply(H.transpose()));		
-		return new QRDecomposition((H.multiply(P_).multiply(H.transpose())).add(R)).getSolver()
-				.solve(P_.multiply(H.transpose()));
+	public RealMatrix updateKalmanGain( SparseRealMatrix H, SparseRealMatrix R, RealMatrix P_ ) {
+		RealMatrix work = H.multiply(P_.transpose());
+		return (new QRDecomposition((work.multiply(H.transpose())).add(R)).getSolver().solve(work)).transpose();
 	}
 	
 	/** 
@@ -70,7 +88,7 @@ public class Filter {
 	/** 
 	 * state: propagate apriori x_
 	 * @param A,Q,x_   */
-	public RealMatrix propagateState( RealMatrix A, RealMatrix x ) {
+	public RealMatrix propagateState( SparseRealMatrix A, RealMatrix x ) {
 		
 		return  A.multiply(x);
 	
@@ -79,7 +97,7 @@ public class Filter {
 	/** 
 	 * covariance: apriori propagate P_
 	 * @param A,Q,P  */
-	public RealMatrix propagateCovariance( RealMatrix A, RealMatrix Q, RealMatrix P ) {
+	public RealMatrix propagateCovariance( SparseRealMatrix A, SparseRealMatrix Q, RealMatrix P ) {
 		
 		return  (A.multiply(P).multiply(A.transpose())).add(Q); 
 	

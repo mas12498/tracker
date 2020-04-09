36e2685871184308e10a5f29198489db50880fa0
Casey <caseyorigami@gmail.com>
2020-04-09 16:39:52 -0700

    added test which compares numerical derivative to analytic position

--- src/test/java/tspi/filter/TestRacetrack.java
+++ src/test/java/tspi/filter/TestRacetrack.java
@@ -3,19 +3,24 @@ package tspi.filter;
 import junit.framework.TestCase;
 import tspi.rotation.Vector3;
 
+/** Trying to make some sanity tests for the 'racetrack' type target simulator. But there are some ideas here that could
+ * apply to arbitrary paths from any simulator; like tests for uniform motion, or closed paths, or basic continuity checks.
+ * TODO add numerical path integrals for velocity and acceleration; should be about zero for uniform, closed motion...
+ * */
 public class TestRacetrack extends TestCase {
     boolean verbose = true;
-
     double radius = 1.0;
     double velocity = 1.0;
     Vector3 c1 = new Vector3(0,0,0);
     Vector3 c2 = new Vector3(0,2,0);
     Racetrack racetrack = new Racetrack(0.0, c1, c2, radius, velocity);
-
     double epsilon = 0.000000000001;
-    int n = 1000;
+    int n = 5000;
     double dt = racetrack.getPerimeter() / n;
+    // TODO tests methods should accept racetrack and some parameters as an argument so we can test a variety of configurations
 
+    /** Make sure successive points within some time bound are within some position bound.
+     * Should be generalized beyond just position and constant velocity cases... */
     public void testContinuity() {
         Vector3 p0 = new Vector3( racetrack.getPositionVector(0.0) );
         double max = velocity * dt + epsilon;
@@ -38,9 +43,10 @@ public class TestRacetrack extends TestCase {
             );
             p0 = p1;
         }
-    } // TODO while this is a good sanity check for Trajectories in general, we could just test the transitions between racetrack piecewise defined geometries...
+    }
 
-    public void testConstantVelocity() {
+    /** ensures the racetrack motion really does have uniform speed */
+    public void testConstantSpeed() {
         for (double t=0; t<racetrack.getPerimeter()*2; t+=dt) {
             Vector3 p = new Vector3( racetrack.getPositionVector(t) );
             Vector3 v = new Vector3( racetrack.getVelocityVector(t) );
@@ -52,4 +58,39 @@ public class TestRacetrack extends TestCase {
             );
         }
     }
+
+    /** the analytic instantaneous velocity should compare well with numerical differentiation.
+     * This should be made into a general test for all trajectories, and acceleration...*/
+    public void testAgainstNumericalDerivative() {
+        // crud, I don't know, how much can we expect velocity and finite differences to agree?
+        double bound = dt*dt*velocity*velocity; // sure, let's go with that. Keep in mind this changes with sampling rates, velocity, the order of the function, etc.
+        // But we should take a closer look sometime...  we also might consider implementing higher order numerical estimates
+
+        // incrementally sample motion
+        Vector3 p0 = new Vector3( racetrack.getPositionVector(0.0) );
+        for (double t=dt; t<racetrack.getPerimeter()*2; t+=dt) {
+
+            // compute finite difference as an estimate for velocity
+            Vector3 p1 = new Vector3( racetrack.getPositionVector(t) );
+            Vector3 dp = new Vector3(p1).subtract(p0);
+
+            // use analytic velocity at midpoint as your truth
+            Vector3 v = new Vector3( racetrack.getVelocityVector(t) );
+
+            // find the error
+            Vector3 e = new Vector3(v).multiply(dt).subtract(dp);
+            double error = e.getAbs();
+
+            if(verbose) System.out.println("e="+e.toTupleString()+", ||e||="+error+", t="+t);
+            //System.out.println("p="+p1.toTupleString()+"v="+v.toTupleString()+", e="+e.toTupleString()+", t="+t);
+
+            assertTrue(
+                    "finite differences varied more than |e|="+bound+" from analytic velocity at t="+t+", e="+e.toTupleString(),
+                    (error<bound)
+            );
+            p0 = p1;
+            v = new Vector3( racetrack.getVelocityVector(t) ); // might consider interpolating velocity...
+        }
+    } // TODO while this is a good sanity check for Trajectories in general, we could just test the transitions between racetrack piecewise defined geometries...
+
 }

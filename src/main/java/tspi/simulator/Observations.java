6b67d0d0ca02e712ef9375ac9def5977b041394a
Casey <caseyorigami@gmail.com>
2020-05-28 23:20:28 -0700

    notes from refactoring conversation

--- src/main/java/tspi/simulator/Observations.java
+++ src/main/java/tspi/simulator/Observations.java
@@ -11,13 +11,28 @@ public interface Observations extends Iterator<Double> {
     /** @return the current time */
     public double getTime();
 
-    /** @return the actual location of the target, or null if not available. */
+    /** @return the actual location of the target in EFG, or null if not available. */
     public Vector3 getTruth();
 
     /** @return a vector of observations, whose order corresponds to the ensemble order */
     public Polar[] getObservations();
+    //TODO instead expose an ensemble here
+    // Make the iterator produce ensembles?
+    // Create Observation class with time, truth, and measurements and have iterator produce that
+    // Or drop iterator interface and write methods for explicitly advancing the stream of observations
 
-    // TODO should observations produce an ensemble?
+    // TODO Ensemble
+    // add more pointing routines to ensemble
+    // add write routing to ensemble
+    // add time to ensemble? no, prob not...
+
+    // TODO TestFilter;
+    //make testFilter use an ensemble instead of an array of Pedestals and PedestalModel
+    //make test filter use Observations interface while driving filter.
+    //add error diagnostic output to TestFilter
+
+    // TODO ObservationsSimulator
+    // Make observation simulator use Ensemble to point instead of pointing them individually
 
     //TODO move mode into pedestal and change interface to provide iterator of Pedestals?
 }

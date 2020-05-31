b125f97d0f9a4056454b42c4e28b9aff2b423174
Casey <caseyorigami@gmail.com>
2020-05-30 22:05:58 -0700

    check off todos

--- src/main/java/tspi/simulator/Observations.java
+++ src/main/java/tspi/simulator/Observations.java
@@ -17,23 +17,5 @@ public interface Observations extends Iterator<Ensemble> {
 
     /** @return an Ensemble of Pedestals pointed at a target */
     public Ensemble getEnsemble();
-    //TODO instead expose an ensemble here
-    // Make the iterator produce ensembles?
-    // Create Observation class with time, truth, and measurements and have iterator produce that
-    // Or drop iterator interface and write methods for explicitly advancing the stream of observations
 
-    // TODO Ensemble
-    // add more pointing routines to ensemble
-    // add write routing to ensemble
-    // add time to ensemble? no, prob not...
-
-    // TODO TestFilter;
-    //make testFilter use an ensemble instead of an array of Pedestals and PedestalModel
-    //make test filter use Observations interface while driving filter.
-    //add error diagnostic output to TestFilter
-
-    // TODO ObservationsSimulator
-    // Make observation simulator use Ensemble to point instead of pointing them individually
-
-    //TODO move mode into pedestal and change interface to provide iterator of Pedestals?
 }
